7c5dc8b4bacbee3523d477fc1ae74af9ae23e920
unknown <18adityajadhav@gmail.com>
2020-04-22 13:53:53 +0530

    SFoxEdited

--- app/src/main/java/com/example/sfoxv2/Main2Activity.java
+++ app/src/main/java/com/example/sfoxv2/Main2Activity.java
@@ -1,7 +1,6 @@
 package com.example.sfoxv2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
--- app/src/main/java/com/example/sfoxv2/Main3Activity.java
+++ app/src/main/java/com/example/sfoxv2/Main3Activity.java
@@ -2,17 +2,25 @@ package com.example.sfoxv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.net.URL;
 
 public class Main3Activity extends AppCompatActivity {
     WebView sfox;
     EditText edittext;
-    String url;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +30,11 @@ public class Main3Activity extends AppCompatActivity {
         sfox = (WebView)findViewById(R.id.web);
         edittext = (EditText)findViewById(R.id.edittext);
 
+        //final Intent intent = new Intent(Main3Activity.this, Main2Activity.class);
 
         WebSettings webSettings = sfox.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setUseWideViewPort(true);
         String site = i.getStringExtra("site");
         String url =  i.getStringExtra("url");
         int flag = Integer.parseInt(i.getStringExtra("flag"));
@@ -43,20 +53,74 @@ public class Main3Activity extends AppCompatActivity {
                 }
             }
             else if(flag==0) {
-                sfox.loadUrl("https://" + url);
+                if(!url.startsWith("www.")&& !url.startsWith("http://") && !url.startsWith("https://"))
+                {
+                    url = "www."+url;
+                }
+                if(!url.startsWith("http://") && !url.startsWith("https://"))
+                {
+                    if(isValid("https://"+url))
+                    {
+                        url = "https://"+url;
+                    }
+                    else if(isValid("http://"+url))
+                    {
+                        url = "http://"+url;
+                    }
+                }
+                sfox.loadUrl(url);
+
             }
 
-
-
-
-        sfox.setWebViewClient(new WebViewClient(){
-            public boolean shouldOverrideUrlLoading(WebView viewx, String url) {
-                viewx.loadUrl(url);
+        edittext.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE))
+                {
+                    String urlstr = edittext.getText().toString();
+                    if(!urlstr.startsWith("www.")&& !urlstr.startsWith("http://") && !urlstr.startsWith("https://"))
+                    {
+                        urlstr = "www."+urlstr;
+                    }
+                    if(!urlstr.startsWith("http://") && !urlstr.startsWith("https://"))
+                    {
+                        if(isValid("https://"+urlstr))
+                        {
+                            urlstr = "https://"+urlstr;
+                        }
+                        else if(isValid("http://"+urlstr))
+                        {
+                            urlstr = "http://"+urlstr;
+                        }
+                    }
+                    sfox.loadUrl(urlstr);
+                }
                 return false;
             }
         });
-        edittext.setText(url);
+        sfox.setWebViewClient(new WebViewClient(){
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
+
+        });
+
+        edittext.setOnFocusChangeListener(new View.OnFocusChangeListener()
+        {
+            @Override
+            public void onFocusChange (View v,boolean hasFocus){
+                if (!hasFocus) {
+                    hideKeyboard(v);
+                }
+            }
+        });
+
     }
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
     public void onBackPressed() {
         if (sfox.canGoBack()) {
             sfox.goBack();
@@ -64,4 +128,18 @@ public class Main3Activity extends AppCompatActivity {
             super.onBackPressed();
         }
     }
+    public boolean isValid(String url)
+    {
+        try
+        {
+            new URL(url).toURI();
+            return true;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+
+
 }
