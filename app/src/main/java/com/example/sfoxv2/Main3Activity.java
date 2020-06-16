bc178a83992c22ae529632feb24f48ef638f1cf9
unknown <18adityajadhav@gmail.com>
2020-06-16 11:49:33 +0530

    SFoxEdited

--- app/src/main/java/com/example/sfoxv2/Main3Activity.java
+++ app/src/main/java/com/example/sfoxv2/Main3Activity.java
@@ -1,6 +1,7 @@
 package com.example.sfoxv2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -19,17 +20,17 @@ import java.net.URL;
 public class Main3Activity extends AppCompatActivity {
     WebView sfox;
     EditText edittext;
+    SwipeRefreshLayout pulltorefresh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
-        getSupportActionBar().hide();
         Intent i = getIntent();
         sfox = (WebView) findViewById(R.id.web);
         edittext = (EditText) findViewById(R.id.edittext);
+        pulltorefresh = (SwipeRefreshLayout) findViewById(R.id.pullToRefresh);
 
-        //final Intent intent = new Intent(Main3Activity.this, Main2Activity.class);
 
         WebSettings webSettings = sfox.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -56,18 +57,26 @@ public class Main3Activity extends AppCompatActivity {
 
             }
         } else if (flag == 0) {
-            if (!url.startsWith("www.") && !url.startsWith("http://") && !url.startsWith("https://")) {
-                url = "www." + url;
-            }
-            if (!url.startsWith("http://") && !url.startsWith("https://")) {
-                if (isValid("https://" + url)) {
-                    url = "https://" + url;
-                } else if (isValid("http://" + url)) {
-                    url = "http://" + url;
+
+            if(url.contains("."))
+            {
+                if (!url.startsWith("www.") && !url.startsWith("http://") && !url.startsWith("https://"))
+                {
+                    url = "www." + url;
+                }
+                if (!url.startsWith("http://") && !url.startsWith("https://")) {
+                    if (isValid("https://" + url)) {
+                        url = "https://" + url;
+                    } else if (isValid("http://" + url)) {
+                        url = "http://" + url;
+                    }
                 }
             }
+            else
+            {
+                url = "https://www.google.com/search?q="+url;
+            }
             sfox.loadUrl(url);
-            //edittext.setText(url);
 
         }
 
@@ -75,18 +84,25 @@ public class Main3Activity extends AppCompatActivity {
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) || (actionId == EditorInfo.IME_ACTION_DONE)) {
                     String urlstr = edittext.getText().toString();
-                    if (!urlstr.startsWith("www.") && !urlstr.startsWith("http://") && !urlstr.startsWith("https://")) {
-                        urlstr = "www." + urlstr;
-                    }
-                    if (!urlstr.startsWith("http://") && !urlstr.startsWith("https://")) {
-                        if (isValid("https://" + urlstr)) {
-                            urlstr = "https://" + urlstr;
-                        } else if (isValid("http://" + urlstr)) {
-                            urlstr = "http://" + urlstr;
+                    if(urlstr.contains("."))
+                    {
+                        if (!urlstr.startsWith("www.") && !urlstr.startsWith("http://") && !urlstr.startsWith("https://"))
+                        {
+                            urlstr = "www." + urlstr;
+                        }
+                        if (!urlstr.startsWith("http://") && !urlstr.startsWith("https://")) {
+                            if (isValid("https://" + urlstr)) {
+                                urlstr = "https://" + urlstr;
+                            } else if (isValid("http://" + urlstr)) {
+                                urlstr = "http://" + urlstr;
+                            }
                         }
                     }
+                    else
+                    {
+                        urlstr = "https://www.google.com/search?q="+urlstr;
+                    }
                     sfox.loadUrl(urlstr);
-                    //edittext.setText(urlstr);
                 }
                 return false;
             }
@@ -120,6 +136,14 @@ public class Main3Activity extends AppCompatActivity {
             }
         });
 
+
+        pulltorefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                sfox.reload();
+                pulltorefresh.setRefreshing(false);
+            }
+        });
     }
 
     public void hideKeyboard(View view) {
@@ -143,5 +167,4 @@ public class Main3Activity extends AppCompatActivity {
             return false;
         }
     }
-
 }
